a870aae3b0129a25782750de42171ed1b76c000c
Swapna-5724 <swapnakoranimath@gmail.com>
2026-04-17 23:09:07 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
-console.log("HELLO FROM OUR FIRST JS FILE");
-let total = 1 + 3;
-console.log("GOODBYE!");
+// console.log("ITS WORKING!!!")
+
+// console.log("BEFORE CONDITIONAL")
+// // Math.random()
+// let random = Math.random();
+// if (1 + 1 == 2) {
+//     console.log("MATH STILL WORKS!")
+// }
+// console.log("AFTER CONDITIONAL")
+
+console.log("BEFORE CONDITIONAL")
+let random = Math.random();
+if (random < 0.5) {
+   console.log("YOUR NUMBER IS LESS THAN 0.5!!!")
+   console.log(random);
+}
+
+if (random >= 0.5) {
+    console.log("YOUR NUMBER IS GREATER THAN 0.5!!!")
+   console.log(random);
+}
+
+
+let rating = 3;
+
+if (rating === 3) {
+    console.log("YOU ARE A SUPERSTAR!");
+}
