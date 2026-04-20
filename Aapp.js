b3d0f2f3c4577e850368b2590bc6f507a8d41823
Swapna-5724 <swapnakoranimath@gmail.com>
2026-04-20 23:09:26 +0530

    Add files via upload

---
+++ Aapp.js
@@ -0,0 +1,50 @@
+//  IN CONSOLE:-
+
+[  // let days = ['Monday', 'Tuesday', 'Wednesday']
+
+// days
+
+// days.length
+
+// [].length
+
+// let KitchenSink = [true, undefined, 12, 9.999, NaN, false, null, 'HIIII!']
+// KitchenSink
+
+// "monday"[0]
+
+// days
+
+// days.length
+
+// days[2]
+// days[300]
+// days[3000]
+// days[1]
+// days[1][0]  ]
+
+
+let firstName = "colt";
+firstName[0]
+firstName[0] ='C'
+firstName
+
+let colors = ['rad', 'orange', 'yallow'];
+colors[0] = 'red'
+colors
+colors[2] = 'Yellow'
+colors
+colors[1] = 70;
+70
+colors
+colors[10] = 'indigo'
+colors
+colors.length
+colors[5]
+colors[6]
+colors[10]
+
+let beatles = ['paul', 'john', 'geaorge']
+beatles
+
+// PUSH AND POP
