7796e1edf1b282efe875b440f44285020476d3b6
Swapna-5724 <swapnakoranimath@gmail.com>
2026-04-23 23:26:17 +0530

    Add files via upload

---
+++ F App.js
@@ -0,0 +1,60 @@
+// video -> 198 Intro to For Loops
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+// console.log(6)
+// console.log(7)
+// console.log(8)
+// console.log(9)
+// console.log(10)
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// for (let num = 1; num <= 10; num++) {
+//     console.log(num);
+// }
+
+// for (let num =1; num <= 10; num +=1) {
+//     console.log("IM IN THE LOOP BODY!!!")
+//     console.log(num);
+// }
+
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+// 10
+
+// video -> 200 THE perils Of infinite Loops
+// DO NOT RUN THIS LOOP
+// INFINITE LOOP!! BAD!!
+// ==============================
+// ==============================
+// for (let i = 20; i >= 0; i++) {
+//     console.log(i)
+// }
+// DO NOT RUN THIS LOOP
+// INFINITE LOOP!! BAD!!
+// ================================
+// ================================
+
+// video :-> 201 Looping Over Arrays
+
+// LIST OF ANIMALS
+
+// for (let i = 0; i < animals.length i++) {
+//     console.log(i, animals[i])
+// }
+
+for (let i = animals.lenght - 1; i >= 0; i--) {
+    console.log(animals[i])
+}
