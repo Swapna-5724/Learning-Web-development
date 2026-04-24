fd960608dec39049e524f85008c6c897e222b707
Swapna-5724 <swapnakoranimath@gmail.com>
2026-04-24 19:06:56 +0530

    Add files via upload

--- WGG app.js
+++ WGG app.js
@@ -1,22 +1,31 @@
 let maximum = parseInt(prompt("Enter the maximum number!"));
-if(!maximum) {
+while (!maximum) {
     maximum = parseInt(prompt("Enter a valid number!"));
 }
 
 // Math.floor(Math.random() * 10) + 1
 const targetNum = Math.floor(Math.random() * maximum) + 1;
-console.log(targetNum);
+// console.log(targetNum);
 
-let guess = parseInt(prompt("Enter Your first guess!"));
+// MATH.FLOOR
+// let guess = parseInt(prompt("Enter Your first guess!"));
+let guess = prompt("Enter Your first guess! (Type 'q' to quit )");
 let attempts = 1;
 
-while (guess !== targetNum) 
-    if (guess === 'q') break;
-    attempts++;
+while (parseInt(guess) !== targetNum) 
+    if (guess === 'q') break;   
+    guess = parseInt(guess);
+    // attempts++;
     if (guess > targetNum) {
         guess = prompt("Too high! Enter a new guess:")
-    } else {
+        attempts++;
+    }
+    else if (guess < targetNum) {
         guess = prompt("Too low! Enter a new guess:")
+        attempts++;
+    }
+    else {
+        guess = prompt("Invalid guess. PLease enter a number or 'q'to quit")
     }
 
 if (guess === 'q') {
@@ -27,4 +36,4 @@ if (guess === 'q') {
 }
 
 // console.log("YOU GOT IT!")
-console.log(`You got it! It took you ${attempts} guesses`)
+// console.log(`You got it! It took you ${attempts} guesses`)
