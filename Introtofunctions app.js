0a0acdd338478ac26c1a29f91693fb098ba2370b
Swapna-5724 <swapnakoranimath@gmail.com>
2026-04-27 21:51:17 +0530

    Add files via upload

---
+++ Introtofunctions app.js
@@ -0,0 +1,82 @@
+// let die1 = Math.floor(Math.random() * 6) + 1;
+// let die2 = Math.floor(Math.random() * 6) + 1;
+// let die3 = Math.floor(Math.random() * 6) + 1;
+// let die4 = Math.floor(Math.random() * 6) + 1;
+// let die5 = Math.floor(Math.random() * 6) + 1;
+// let die6 = Math.floor(Math.random() * 6) + 1;
+
+// let die1 = rollDie(6);
+// let die2 = rollDie(20);
+// let die3 = rollDie(20);
+// let die4 = rollDie(12);
+// let die5 = rollDie();
+// let die6 = rollDie();
+
+
+// video :-> 213 
+
+// function funcName() {
+//     //do something
+// }
+
+// function singSong() {
+//     console.log("DO");
+//     console.log("RE");
+//     console.log("MI");
+// }
+
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+
+// video :-> 214 Arguments Intro
+
+function greet() {
+    console.log("HI!")
+}
+
+
+function greet(firstName) {
+    console.log(`firstName is: ${firstName}`)
+    console.log("HI!")
+}
+
+// video :->  Functons with Multiple Arguments
+
+     // greet('Clooney', 'George')
+
+function greet(firstName, lastName) {
+    console.log(`Hey there, ${firstName} ${lastName[0]}.`)
+}
+
+function repeat(str, numTimes) {
+    let result = '';
+    for (let i = 0; i < numTimes; i++) {
+        result += str;
+    }
+    console.log(result);
+}
+
+
+// video :-> 216 The Return Keyword
+
+// IN CONSOLE
+        // [
+        //     add(9,4)
+        //      add(9, 'a')
+        //      add(6, 9)
+        //      let total = add (6, 9)
+        //      let total = add(9,4)
+        //      total
+        //      let r = Math.random()
+        //     
+        // ]
