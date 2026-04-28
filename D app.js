c2c44d43c8dd2d909b0ad5cba761683e06215292
Swapna-5724 <swapnakoranimath@gmail.com>
2026-04-28 23:00:27 +0530

    Add files via upload

---
+++ D app.js
@@ -0,0 +1,38 @@
+const myMath = {
+    PI: 3.14159,
+    // square: function (num) {
+    square(num) {
+        return num * num;
+    },
+    // cube: function (num) {
+    cube(num) {
+        return num ** 3;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// IN CONSOLE:
+ASKDJAS
+"hello".toUpperCase()
+[1,2,3].indexOf(2)
+function add(x, y) {return x+y}
+add(1,2)
+Math
+
+myMath
+myMath.PI
+myMath.square
+myMath.square(2)
+myMath.cube(2)
+myMath["cube"](4)
