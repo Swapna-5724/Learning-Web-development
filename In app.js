947365ba65bf393c5217a660b1e6c563e89eec67
Swapna-5724 <swapnakoranimath@gmail.com>
2026-05-07 23:40:56 +0530

    Add files via upload

--- In app.js
+++ In app.js
@@ -200,6 +200,90 @@ const total = prices.reduce((total, price) => {
 
 const total = prices.reduce((total, price) => total + price)
 
-const prices.reduce((min, price) => {
-    
+const prices = prices.reduce((min, price) => {
+    if (price < min) {
+    // if (price > min) {
+        return price;
+    }
+    return min;
 })
+
+
+const movies = [
+    {
+        title: 'Amadeus'
+        score: 99,
+        year: 1984
+    },
+    {
+        title: 'Shaknado',
+        score: 35,
+        year: 2013
+    },
+    {
+        title: '13 Going On 30'
+        score: 70,
+        year: 2004
+    },
+    {
+        title: 'Stand By Me',
+        score: 85,
+        year: 1986
+    },
+    {
+        title: 'Waterworld',
+        score: 62,
+        year: 1995
+    },
+    {
+        title: 'Jingle All The Way',
+        score: 71,
+        year: 1996
+    },
+    {
+        title: 'Parasite',
+        score: 95,
+        year: 2019
+    },
+    {
+        title: 'Notting Hill',
+        score: 77,
+        year: 1999
+    },
+    {
+        title: 'Alien',
+        score: 90,
+        year: 1979
+    }
+]
+
+
+const highestRated = movies.reduce((bestMovie, currMovie) => {
+    if (currMovie.score > bestMovie.score) {
+        return currMovie;
+    }
+    return bestMovie;
+})
+
+const evens = [2, 4, 6, 8];
+evens.reduce((sum, num) => sum + num, 100)
+
+
+// video :-> 238 Arrow Functions & 'this'
+
+const person = {
+    firstName: 'Viggo',
+    lastName: 'Mortensen',
+    // fullName: function()
+    fullName: () => {
+        // console.log(this);
+        return `${this.firstName} ${this.lastName}`
+    },
+    shoutName: function() {
+        // setTimeout(function() {
+        setTimeout(() => {
+            console.log(this);
+            console.log(this.fullName())
+        }, 3000)
+    }
+}
