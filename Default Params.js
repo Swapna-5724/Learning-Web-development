11fee6e017b759b96610b612225f474ec34d3338
Swapna-5724 <swapnakoranimath@gmail.com>
2026-05-08 22:22:41 +0530

    Add files via upload

--- Default Params.js
+++ Default Params.js
@@ -21,4 +21,26 @@ function greet(person, msg = "Hey there", punc = '!') {
     console.log(`${msg}, ${person}!`)
 }
 
+function greet(person, msg = "Hey there", punc = '!') {
+    console.log(`${msg}, ${person}${punc}`)
+}
+
 ]
+
+// video :-> 241  spread in function calls
+
+            // [
+            //     IN console web Page:
+            //     Math.max(13,4,5,21,3,3,1,2,7,6,4,2,53456)
+            //     Math.min(2,5,1)
+            //     const nums = [13,4,5,21,3,3,1,2,7,6,4,2,53456]
+            //     Math.max(nums)
+            //     Math.max(...nums)
+            //     Math.min(...nums)
+            //     console.log("asd", "as", "asdasd", "asdsa")
+            //     console.log(nums)
+            //     console.log(...nums)
+            //     console.log('hello')
+            //     console.log(...'hello')
+            //     console.log('h', 'e', 'l')
+            // ]
