cce6cdb22a060ff92729037ea26d94d89925283c
Swapna-5724 <swapnakoranimath@gmail.com>
2026-05-13 21:21:07 +0530

    Add files via upload

---
+++ GetElementByID.js
@@ -0,0 +1,24 @@
+#document
+#document
+document.getElementById('chicken')
+document.getElementById('checken')
+document.getElementById('banner')
+const banner = document.getElementById('banner')
+console.dir(banner)
+
+document.getElementById('toc')
+const toc = document.getElementById('toc')
+console.dir(toc)
+
+// Video :-> 252 getElementByIdTagName & className
+
+document.getElementsById('img')
+const allImages = document.getElementByIdTagName('img')
+allImages
+allImages[0]
+
+console.dir(allImages[1])
+allImages.map
+allImages.askdkjaslkdjas
+allImages.length
+const allImages = document.getElementByTagName('img')
