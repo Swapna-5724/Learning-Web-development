3731424a6decf053bd9e39b16733faebff9c76d6
Swapna-5724 <swapnakoranimath@gmail.com>
2026-05-15 22:35:41 +0530

    Add files via upload

--- GetElementByID.js
+++ GetElementByID.js
@@ -10,18 +10,48 @@ document.getElementById('toc')
 const toc = document.getElementById('toc')
 console.dir(toc)
 
+                                                                    // Video :-> 252 getElementByIdTagName & className
+
+                                                                    document.getElementsById('img')
+                                                                    const allImages = document.getElementByIdTagName('img')
+                                                                    allImages
+                                                                    allImages[0]
+
+                                                                    console.dir(allImages[1])
+                                                                    allImages.map
+                                                                    allImages.askdkjaslkdjas
+                                                                    allImages.length
+                                                                    const allImages = document.getElementByTagName('img')
+
+
+
 // Video :-> 252 getElementByIdTagName & className
 
-document.getElementsById('img')
-const allImages = document.getElementByIdTagName('img')
-allImages
-allImages[0]
+const allImages = document.getElementByTagName('img');
 
-console.dir(allImages[1])
-allImages.map
-allImages.askdkjaslkdjas
-allImages.length
-const allImages = document.getElementByTagName('img')
+for (let img of allImages) {
+    img.src = 
+}
+
+const squareImages = document.getElementsByClassName('square');
+
+for (let img of squareImages) {
+    img.src = 'https://upload.wikipedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg';
+}
+
+
+//  in console:
+
+documnet.getElementByIdClassName('sqardsd')
+document.getElementById('asdsd')
 
 
 
+//   video:-> 253  querySelector &querySelectorAll
+
+
+
+// In console:
+
+document.querySelector('p')
+document.getElementById
