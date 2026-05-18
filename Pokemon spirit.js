a44fd9f13ddd7fb0150ce187a97fd1ac6b4400d7
Swapna-5724 <swapnakoranimath@gmail.com>
2026-05-18 22:23:46 +0530

    Add files via upload

---
+++ Pokemon spirit.js
@@ -0,0 +1,6 @@
+// https://raw.githubusercontent.com/PokeAPI/spirites/master/sprites/pokemon/1.png
+
+const container = document.querySelector('#conatiner');
+const newImg = document.createElement('img');
+newImg.src = 'https://raw.githubusercontent.com/PokeAPI/spirites/master/sprites/pokemon/1.png';
+conatiner.appendChild(newImg)
