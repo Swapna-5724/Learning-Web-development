aa5f6e9c34356b156897a706d5475b0746fb601f
Swapna-5724 <swapnakoranimath@gmail.com>
2026-05-27 00:01:56 +0530

    Add files via upload

--- Events and The keyword this.js
+++ Events and The keyword this.js
@@ -1,11 +1,11 @@
 const makeRandomCOlor = () => {
     const r = math.floor(Math.random() * 255);
-    const r = math.floor(Math.random() * 255);
-    const r = math.floor(Math.random() * 255);
+    const g = math.floor(Math.random() * 255);
+    const b = math.floor(Math.random() * 255);
     return `rgb(${r}, ${g}, ${b})`;
 }
 
-const button = docummnet.querySelectorAll('button');
+// const button = documnet.querySelectorAll('button');
 
 for (let button of buttons) {
     button.addEventListener('click', function () {
@@ -15,22 +15,22 @@ for (let button of buttons) {
     })
 }
 
-for (let this of buttons) {
-    this.addEventListener('click', function () {
-        // console.log("CLICKED")
-        this.style.backgroundColor = makeRandomColor();
-        this.style.color = makeRandomColor();
-    })
-}
+// for (let this of buttons) {
+//     this.addEventListener('click', function () {
+//         // console.log("CLICKED")
+//         this.style.backgroundColor = makeRandomColor();
+//         this.style.color = makeRandomColor();
+//     })
+// }
 
 const h1s = document.querySelectorAll('h1');
 for (let h1 of h1s) {
     h1.addEventListener('click', function () {
         console.log(this)
         this.style.backgroundColor = makeRandomColor();
+         this.style.color = makeRandColor();
         // h1.style.backgroundColor = makeRandomColor();
         // h1.style.color = makeRandColor();
-        this.style.color = makeRandColor();
     })
 }
 
@@ -46,12 +46,12 @@ function colorize() {
 
 
 
-for (let this of buttons) {
-    button.addEventListener('click', colorize)
-    }
+// for (let this of buttons) {
+//     button.addEventListener('click', colorize)
+//     }
 
 
-const h1s = document.querySelectorAll('h1');
-for (let h1 of h1s) {
-    h1.addEventListener('click', colorize) 
-    }
+// const h1s = document.querySelectorAll('h1');
+// for (let h1 of h1s) {
+//     h1.addEventListener('click', colorize) 
+//     }
