9ff6fb8ea7d7d98315d1a527aa7609bac384b3cb
Swapna-5724 <swapnakoranimath@gmail.com>
2026-05-26 23:08:05 +0530

    Add files via upload

---
+++ Events and The keyword this.js
@@ -0,0 +1,57 @@
+const makeRandomCOlor = () => {
+    const r = math.floor(Math.random() * 255);
+    const r = math.floor(Math.random() * 255);
+    const r = math.floor(Math.random() * 255);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+const button = docummnet.querySelectorAll('button');
+
+for (let button of buttons) {
+    button.addEventListener('click', function () {
+        // console.log("CLICKED")
+        button.style.backgroundColor = makeRandomColor();
+        button.style.color = makeRandomColor();
+    })
+}
+
+for (let this of buttons) {
+    this.addEventListener('click', function () {
+        // console.log("CLICKED")
+        this.style.backgroundColor = makeRandomColor();
+        this.style.color = makeRandomColor();
+    })
+}
+
+const h1s = document.querySelectorAll('h1');
+for (let h1 of h1s) {
+    h1.addEventListener('click', function () {
+        console.log(this)
+        this.style.backgroundColor = makeRandomColor();
+        // h1.style.backgroundColor = makeRandomColor();
+        // h1.style.color = makeRandColor();
+        this.style.color = makeRandColor();
+    })
+}
+
+
+
+
+
+
+function colorize() {
+    
+}
+
+
+
+
+for (let this of buttons) {
+    button.addEventListener('click', colorize)
+    }
+
+
+const h1s = document.querySelectorAll('h1');
+for (let h1 of h1s) {
+    h1.addEventListener('click', colorize) 
+    }
