05bc7cee464659e6545f7704f9e0841a2b800bb0
Swapna-5724 <swapnakoranimath@gmail.com>
2026-06-04 00:07:50 +0530

    Add files via upload

--- Form Events and Prvnt default.js
+++ Form Events and Prvnt default.js
@@ -1,6 +1,14 @@
 const form = document.querySelector("#shelterForm");
-form.addEventListener("submit", function(e){
+const input = document.querySelector("#catName");
+const list = document.querySelector("#cats");
+form.addEventListener("submit", function(e) {
     // console.log("SUBMITTED!!!");
     e.preventDefault();
-    console.log("SUMITTED THE FORM!")
+    // console.log("SUMITTED THE FORM!")
+    const catName = input.value;
+    const newLI = document.createElement("LI");
+    newLI.innerText = catName;
+    list.appendChild(newLI);
+    input.value = "";
 });
+
