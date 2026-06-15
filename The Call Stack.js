2a9d83ecfa42869b34fe29d653ddd1b184c25850
Swapna-5724 <swapnakoranimath@gmail.com>
2026-06-15 22:32:57 +0530

    Add files via upload

---
+++ The Call Stack.js
@@ -0,0 +1,30 @@
+const multiply = (x, y) => x * y;
+
+const square = x => multiply(x, x);
+
+const isRightTriangle = (a, b, c) => (
+    square(a) + square(b) === square(c)
+)
+console.log("BEFORE")
+isRightTriangle(3,4,5)
+
+console.log("DONEEEE!")
+
+
+
+
+
+
+
+// latenflip.com/loupe/    ->   In Google
+
+
+
+
+
+
+
+// IN CONSOLE:
+
+isRightTriangle(3,4,5)
+isRightTriangle(3,4,8)
