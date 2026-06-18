e7e0caf02fb3572924af53243eac3dc82665102d
Swapna-5724 <swapnakoranimath@gmail.com>
2026-06-18 23:47:44 +0530

    Add files via upload

--- Fakerequestprmise.js
+++ Fakerequestprmise.js
@@ -76,4 +76,53 @@ const fakeRequestPromise = (url) => {
 
 
 
-fakeRequestPromise('yelp.com/api/coffee')
+const request = fakeRequestPromise('yelp.com/api/coffee/page1');
+request
+    .then(() => {
+        // console.log("PROMISE RESOLVED!")
+        console.log("IT WORKED!!!!! (page1)")
+        fakeRequestPromise('yelp.com/api/coffee/page2')
+            .then(() => {
+                // console.log("PROMISE RESOLVED (2)!")
+                // console.log("IT WORKED!!!!!!!!")
+                console.log("IT WORKED!!!!! (page2)")
+                fakeRequestPromise('yelp.com/api/coffee/page3')
+                    .then(() => {
+                        console.log("IT WORKED!!!! (page3)")
+                    })
+                    .catch(() => {
+                        console.log("OH NO, ERROR!!!! (Page3)")
+                    })
+            })
+             .catch(() => {
+                // console.log("PROMISE REJECTED!")
+                // console.log("OH NO, ERROR!!!!")
+                console.log("OH NO, ERROR!!!! (Page2)")
+    })
+    })
+    .catch(() => {
+        // console.log("PROMISE REJECTED!")
+        // console.log("OH NO, ERROR!!!!")
+        console.log("OH NO, ERROR!!!! (Page1)")
+    })
+
+// request.then(() => {
+//     console.log("IT WORKED!!!!!!!!")
+// }).catch(() => {
+//     console.log("OH NO, ERROR!!!!")
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+// // IN CONSOLE:
+// response
+// request
