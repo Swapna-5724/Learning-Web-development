a8180b73c6708da2b933f7d5adeb00b09d853564
Swapna-5724 <swapnakoranimath@gmail.com>
2026-06-25 22:57:04 +0530

    Add files via upload

---
+++ What is JSON.js
@@ -0,0 +1,13 @@
+// THIS IS A STRING OF JSON
+const data = `{"ticker":{"base":"BTC","target":"USD", "price":"11288.49813464","volume":"91769.69699773","change":"-46.29462447"},"timestamp":1596510482,"success":true,"error":""}`
+
+
+// IN CONSOLE:
+
+// JSON.parse(data)
+// const parseData = JSON.parse(data)
+// parseData.ticker
+// parseData.ticker.price
+
+// const dog = {breed:'lab', color: 'black', isALive: true, owner: undefined}
+// JSON.stringify(dog)
